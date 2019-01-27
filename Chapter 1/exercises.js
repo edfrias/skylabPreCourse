a4b56868ef63258b29e834227bc87f434b452b96
Edgar Frías <edfriasji@gmail.com>
2019-01-27 23:37:38 +0100

    Last part of exercises from chapter 1

--- Chapter 1/exercises.js
+++ Chapter 1/exercises.js
@@ -29,3 +29,79 @@ const age = (my_age) => {
 }
 
 console.log(name('Edgar ') + age(36));
+
+// g
+const my_data = (my_name, my_age) => {
+  const name = (my_name) => {
+    return my_name;
+  }
+
+  const age = (my_age) => {
+    return my_age;
+  }
+
+  return name(my_name) + age(my_age);
+}
+
+my_data('Edgar ', 36);
+
+// h
+const my_data = (my_name) => {
+  const name = (my_name) => {
+    return my_name;
+  }
+
+  const randomized = () => {
+    return Math.trunc(Math.random() * 100);
+  }
+
+  const age = (my_age = randomized()) => {
+    return my_age;
+  }
+
+  return name(my_name) + age();
+}
+
+my_data('Edgar ');
+
+// i + j
+const my_data = (my_name) => {
+  const name = (my_name) => {
+    return `${my_name}...aka IRONMAN`;
+  }
+
+  const randomized = () => {
+    return Math.trunc(Math.random() * 100);
+  }
+
+  const age = (my_age = randomized()) => {
+    return `${my_age}...Sure you're Tony Stark?`;
+  }
+
+  return `The first function returns: ${name(my_name)}, The second function returns: ${age()}`;
+}
+
+my_data('Edgar');
+
+// k
+const my_data = (my_name) => {
+  const name = (my_name) => {
+    return my_name === 'Edgar' ? `The first function returns: ${my_name}...aka IRONMAN` : `The first function returns: ${my_name}... You're not IRONMAN!`;
+  }
+
+  const randomized = () => {
+    return Math.trunc(Math.random() * 100);
+  }
+
+  const age = (my_age = randomized()) => {
+    return `${my_age}...Sure you're Tony Stark?`;
+  }
+
+  if (my_name === 'Edgar') {
+    return `${name(my_name)}, The second function returns: ${age()}`
+  } else {
+    return name(my_name);
+  }
+}
+
+my_data('Gumersindo');
