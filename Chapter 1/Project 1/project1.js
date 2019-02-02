bcc4cfc33d0659ca929928c268a6cb83e7fd65c7
Edgar Frías <edfriasji@gmail.com>
2019-02-02 23:19:08 +0100

    Project 1 - Last version of project 1
    By now the project 1 accepts multiple arguments as in the pro version
    proposal.

--- Chapter 1/Project 1/project1.js
+++ Chapter 1/Project 1/project1.js
@@ -1,16 +1,30 @@
 // Project 1 - Calculator
 const calculator = (...newArgs) => {
-  console.log('the arguments passed', newArgs);
-  console.log('the long of newArgs', newArgs.length);
+  let results = [];
+  let isInvalid = false;
+
   for (let i = 0; i < newArgs.length; i++) {
-    console.log('entering the for');
     if (typeof newArgs[i] !== 'number' || newArgs[i] === null || newArgs[i] === undefined) {
-      console.log('Sorry you entered an invalid input');
-    } else if (newArgs.length === 1) {
-      const oneParamResult = Math.sqrt(newArgs[i]);
-      console.log(`As you only entered one input I can only give the square root of ${newArgs[i]}, that is: ${oneParamResult}`);
-    } else {
-      console.log('params ->', newArgs);
+      isInvalid = true;
     }
+  }
+
+  if (newArgs.length <= 1 && isInvalid) {
+    console.log('Sorry you entered an invalid input');
+  } else if (newArgs.length === 1 && !isInvalid) {
+    const oneParamResult = Math.sqrt(newArgs[0]);
+    console.log(`As you only entered one input I can only give the square root of ${newArgs[0]}, that is: ${oneParamResult % 1 !== 0 ? oneParamResult.toFixed(3) : oneParamResult}`);
+  } else {
+    const summatory = newArgs.reduce((a, b) => a + b);
+    const subtract = newArgs.reduce((a, b) => a - b);
+    const multiply = newArgs.reduce((a, b) => a * b);
+    const division = newArgs.reduce((a, b) => a / b);
+
+    results.push(`The summatory of ${newArgs} is: ${summatory % 1 !== 0 ? summatory.toFixed(3) : summatory},`);
+    results.push(`the subtraction of ${newArgs} is: ${subtract % 1 !== 0 ? subtract.toFixed(3) : subtract},`);
+    results.push(`the multiply of ${newArgs} is: ${multiply % 1 !== 0 ? multiply.toFixed(3) : multiply},`);
+    results.push(`finally the division of ${newArgs} is: ${division % 1 !== 0 ? division.toFixed(3) : division}.`);
+
+    console.log(results.join(' '));
   }
 }
