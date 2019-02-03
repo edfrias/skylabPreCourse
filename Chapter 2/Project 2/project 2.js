b62d73d3bab022597b4e58e935a9ca759b5c8f15
Edgar Frías <edfriasji@gmail.com>
2019-02-03 20:48:42 +0100

    Project 2 - Skylab airlines
    Basic part for the project 2 exercise.

---
+++ Chapter 2/Project 2/project 2.js
@@ -0,0 +1,46 @@
+// Project 2 - Skylab Airlines
+var flights = [
+  { id: 00, to: 'Bilbao', from: 'Barcelona', cost: 1600, scale: false },
+  { id: 01, to: 'New York', from: 'Barcelona', cost: 700, scale: false },
+  { id: 02, to: 'Los Angeles', from: 'Madrid', cost: 1100, scale: true },
+  { id: 03, to: 'Paris', from: 'Barcelona', cost: 210, scale: false },
+  { id: 04, to: 'Roma', from: 'Barcelona', cost: 150, scale: false },
+  { id: 05, to: 'London', from: 'Madrid', cost: 200, scale: false },
+  { id: 06, to: 'Madrid', from: 'Barcelona', cost: 90, scale: false },
+  { id: 07, to: 'Tokyo', from: 'Madrid', cost: 1500, scale: true },
+  { id: 08, to: 'Shangai', from: 'Barcelona', cost: 800, scale: true },
+  { id: 09, to: 'Sydney', from: 'Barcelona', cost: 150, scale: true },
+  { id: 10, to: 'Tel-Aviv', from: 'Madrid', cost: 150, scale: false }
+];
+
+let user = prompt("What's your name?");
+console.log(`Hello ${user}, nice to see you!`);
+console.log('---');
+
+for (let i = 0; i < flights.length; i++) {
+  console.log(`The flight with an origin from: ${flights[i].from} to destination ${flights[i].to}, costs ${flights[i].cost} ${flights[i].scale ? 'and has stopovers' : 'and no stopovers'}.`);
+}
+
+let summatory = 0;
+
+for(let j = 0; j < flights.length; j++){
+  summatory += parseInt(flights[j].cost, 10 );
+}
+
+const average = summatory/flights.length;
+console.log('---');
+console.log(`The average cost for today's flights is ${average}`);
+console.log('---');
+let hasScales = 0;
+
+for (let x = 0; x < flights.length; x++) {
+  if (flights[x].scale === true) {
+    hasScales += 1;
+  }
+}
+console.log(`There are ${hasScales} flights with scales`);
+console.log('---');
+const lastFlights = flights.slice(Math.max(flights.length - 5));
+const lastFlightsDestination = lastFlights.map(flight => flight.to);
+
+console.log(`The last flights for tonight are: ${lastFlightsDestination.join(', ')}`);
