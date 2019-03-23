578556499661b6cf629816371943a4ccd35398b4
Edgar Frías <edfriasji@gmail.com>
2019-03-23 15:51:56 +0100

    Pasapalabra game
    Pasapalabra game

--- Chapter 4/pasapalabra/index.js
+++ Chapter 4/pasapalabra/index.js
@@ -28,17 +28,81 @@ const questions = [
   { letter: 'z', answer: 'zarpar', status: 0, question: 'CON LA Z. Empezar a navegar'},
 ];
 
-let marcador = 0;
+const score = 0;
+const incorrect = 0;
+const skippedQuestions = 0;
 
-for (let quest of questions) {
-  let makeQuestion = prompt(quest.question);
-  if (makeQuestion === 'pasapalabra') {
-    console.log('End of the game!');
-  } else if (makeQuestion === quest.answer) {
-    marcador += 1;
-  } else {
-    console.log('fail!');
+let rankingByPlayer = [];
+
+// Generates player name
+const generatePlayer = (userNum) => {
+  let user = prompt('What is your name?');
+  if (user === '') {
+    user = `Player${userNum}`;
+  }
+
+  return user;
+}
+
+// Check nswered questions by player
+const checkAnswers = (user) => {
+  let userStats = [];
+  let questionsByPlayer = [];
+
+  userStats.push({user: user});
+
+  for (let quest of questions) {
+    let makeQuestion = prompt(`${user}: ${quest.question}`).toLowerCase();
+    const isIncorrect = makeQuestion !== quest.answer;
+
+    // Status could be: 0: not answered, 1: correct, 2: skipped, 3: errored
+    if (isIncorrect) {
+      if (makeQuestion === 'pasapalabra') {
+        questionsByPlayer.push({letter: quest.letter, status: 2});
+      } else {
+        questionsByPlayer.push({letter: quest.letter, status: 3});
+      }
+    } else {
+      questionsByPlayer.push({letter: quest.letter, status: 1});
+    }
+  }
+
+  userStats.push(questionsByPlayer);
+
+  rankingByPlayer.push(userStats);
+
+  return rankingByPlayer;
+}
+
+// Order the ranking given the corrects questions answered
+const orderRanking = () => {
+  rankingByPlayer.sort((a, b) => (a.status1 < b.status1) ? 1 : -1);
+}
+
+// Print the ranking in a friendly way
+const printRanking = () => {
+  orderRanking();
+
+  for (let r = 0; r < rankingByPlayer.length; r++) {
+    const playerName = rankingByPlayer[r][0].user;
+    const totalStatus1 = rankingByPlayer[r][1].filter(status1 => status1.status === 1).length;
+    const totalStatus2 = rankingByPlayer[r][1].filter(status2 => status2.status === 2).length;
+    const totalStatus3 = rankingByPlayer[r][1].filter(status3 => status3.status === 3).length;
+
+    console.log(`${playerName} has ${totalStatus1} point/s and ${totalStatus3} incorrect words and ${totalStatus2} skipped words from a total of ${questions.length - totalStatus2} words`);
   }
 }
 
-console.log('marcador >', marcador);
+// The game itself
+const pasapalabra = () => {
+  const numPlayers = prompt('How many players are you?');
+
+  for (let i = 0; i < numPlayers; i++) {
+    let userName = generatePlayer(i+1);
+    checkAnswers(userName);
+  }
+
+  printRanking();
+}
+
+pasapalabra();
